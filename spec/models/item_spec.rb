a61de6cffe396f88a143251be931cd1ad1ede82c
Caroline Powell <carpowell10@gmail.com>
2017-01-04 08:38:05 -0700

    Fix hound spacing issues, delete autogenerated item factory file

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 describe Item do
   context 'validations' do
-    it {is_expected.to validate_presence_of(:description)}
-    it {is_expected.to validate_presence_of(:title)}
-    it {is_expected.to validate_presence_of(:price_in_cents)}
-    it {is_expected.to validate_presence_of(:image_url)}
-    it {is_expected.to validate_uniqueness_of(:title)}
-    it {is_expected.to validate_uniqueness_of(:image_url)}
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:price_in_cents) }
+    it { is_expected.to validate_presence_of(:image_url) }
+    it { is_expected.to validate_uniqueness_of(:title) }
+    it { is_expected.to validate_uniqueness_of(:image_url) }
   end
 
   context "associations" do
-    it {is_expected.to belong_to(:category)}
+    it { is_expected.to belong_to(:category) }
   end
 end
