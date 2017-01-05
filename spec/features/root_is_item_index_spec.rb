44b66b39d64ee9be19dd4e212357c57b6895f06b
Caroline Powell <carpowell10@gmail.com>
2017-01-04 22:47:38 -0700

    Fix test that looks for 'Timber Shop' to look for upcase

--- spec/features/root_is_item_index_spec.rb
+++ spec/features/root_is_item_index_spec.rb
@@ -4,6 +4,6 @@ RSpec.feature "When a user visits root", type: :feature do
   it "can see index view" do
     visit root_path
 
-    expect(page).to have_content("Timber Shop")
+    expect(page).to have_content("Timber Shop".upcase)
   end
 end
