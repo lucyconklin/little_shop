4fc3d01a109dbebc6c3735cd191918b3a5a89ee8
Robbie Smith <Robbie-Smith@users.noreply.github.com>
2017-01-04 19:45:23 -0700

    Update cart_spec.rb
    Removed commented out code.

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
 describe Cart do
-  # it 'the cart be able to contain items' do
-  #   cart = Cart.new
-  #   cart.contents =  create_list(:item, 5)
-  #
-  #   expect(cart.contents.count).to eq(5)
-  # end
 
   describe "#number_of_items" do
     it "returns the total number of items in the cart" do
