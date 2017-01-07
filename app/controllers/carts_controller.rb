b6ece8734e2cf1a7e7bf4e4f41c3454549ea45b2
Robbie Smith <robert.smith160@icloud.com>
2017-01-06 19:21:20 -0700

    Add increase and decrease functionality

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -13,10 +13,20 @@ class CartsController < ApplicationController
   end
 
   def destroy
+    byebug
     item = Item.find(params[:item_id])
     @cart.remove_item(item.id)
     session[:cart] = @cart.contents
-    flash[:success] = "Succesfully removed #{view_context.link_to(item.title, item)} from your cart."
+    path = request.env['PATH_INFO']
+    unless path.eql?("/cart")
+      flash[:success] = "Succesfully removed #{view_context.link_to(item.title, item)} from your cart."
+    end
+    redirect_to cart_path
+  end
+
+  def update
+    item = Item.find(params[:item_id])
+    @cart.add_item(item.id)
     redirect_to cart_path
   end
 end
