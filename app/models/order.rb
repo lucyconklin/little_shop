8a7e0fe18e32e0f4a2815f344a6107aa5fe80d12
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-01-07 15:24:10 -0700

    Create Orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,20 @@
 class Order < ApplicationRecord
 
   validates :total_price_in_cents, presence: true
+  validates :status, presence: true
+  validates :customer, presence: true
+  validates :items, presence: true
 
   belongs_to :status
   belongs_to :customer
   has_many :order_items
   has_many :items, through: :order_items
+
+  def total_price_in_dollars
+    total_price_in_cents / 100.to_f
+  end
+
+  def date
+    created_at.strftime("%e %b %Y")
+  end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,8 +1,5 @@
 class OrderItem < ApplicationRecord
 
-  validates :item_price_in_cents, presence: true
-  validates :item_quantity, presence: true
-
   belongs_to :item
   belongs_to :order
 end
