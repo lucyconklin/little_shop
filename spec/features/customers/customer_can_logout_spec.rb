20cc1670662f9bcb7de8b76f3391c365d9632500
Caroline Powell <carpowell10@gmail.com>
2017-01-07 17:33:12 -0700

    Replace Signin with Sign in

--- spec/features/customers/customer_can_logout_spec.rb
+++ spec/features/customers/customer_can_logout_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
 feature "When a customer is logged in" do
-  scenario "they can Log out" do
+  scenario "they can log out" do
   end
 end
--- spec/features/customers/visitor_can_login_spec.rb
+++ spec/features/customers/visitor_can_login_spec.rb
@@ -10,10 +10,10 @@ feature "When a visitor visits the root path" do
     click_on "Sign in"
   end
 
-  scenario "they can navigate to the login page and login" do
+  scenario "they can navigate to the login page and log in" do
     fill_in "email", with: "jane@jane.com"
     fill_in "password", with: "boom"
-    click_on "Login"
+    click_on "Log in"
 
     expect(current_path).to eql(dashboard_path)
     expect(page).to have_css(".navbar", text: "Logged in as Jane Doe")
@@ -21,20 +21,20 @@ feature "When a visitor visits the root path" do
 
   scenario "they see an error message if they leave the email blank" do
     fill_in "password", with: "boom"
-    click_on "Login"
+    click_on "Log in"
 
     expect(page).to have_content("Email and password combination do not exist")
   end
 
   scenario "they see an error message if they leave the password blank" do
     fill_in "email", with: "jane@jane.com"
-    click_on "Login"
+    click_on "Log in"
 
     expect(page).to have_content("Email and password combination do not exist")
   end
 
   scenario "they see an error message if they leave both the email and password blank" do
-    click_on "Login"
+    click_on "Log in"
 
     expect(page).to have_content("Email and password combination do not exist")
   end
@@ -42,7 +42,7 @@ feature "When a visitor visits the root path" do
   scenario "they see an error message if they input an invalid email/password combination" do
     fill_in "email", with: "john@john.com"
     fill_in "password", with: "boom"
-    click_on "Login"
+    click_on "Log in"
 
     expect(page).to have_content("Email and password combination do not exist")
   end
