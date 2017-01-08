558496c0615b242b193141a7b848bcc266efeb58
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-01-07 17:29:15 -0700

    Seeds file was making too many items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,12 @@ Category.destroy_all
 Status.destroy_all
 Customer.destroy_all
 
-3.times do
-  category = Category.create!(name: Faker::Commerce.department)
-  9.times do
-    category.items << FactoryGirl.create(:item)
-  end
+FactoryGirl.create_list(:category, 7)
+categories = Category.all
+number_from_20_to_40 = rand(20) + 20
+
+categories.each do |category|
+  category.items << FactoryGirl.create_list(:item, number_from_20_to_40, category: category)
 end
 
 status_names = ["ordered", "paid", "cancelled", "completed"]
@@ -18,14 +19,15 @@ status_names.each do |name|
 end
 
 FactoryGirl.create_list(:customer, 111)
-
 customers = Customer.all
+
 items = Item.all
-statuses = Status.all
-number_between_one_and_five = rand(5) + 1
 
 customers.each do |customer|
-  number_between_one_and_five.times do
-    customer.orders << FactoryGirl.create(:order)
+  number_between_1_and_5 = rand(5) + 1
+  number_between_1_and_9 = rand(9) + 1
+
+  number_between_1_and_9.times do
+    customer.orders << FactoryGirl.create(:order, items: items.sample(number_between_1_and_5))
   end
 end
