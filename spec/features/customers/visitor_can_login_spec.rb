332ffe720e56da4f80388652cb12431edf5e295c
Caroline Powell <carpowell10@gmail.com>
2017-01-07 20:15:41 -0700

    Fix ambigous Log in match on tests, test and functionality for customer log out, tests for flash messages

--- spec/features/customers/visitor_can_login_spec.rb
+++ spec/features/customers/visitor_can_login_spec.rb
@@ -7,44 +7,59 @@ feature "When a visitor visits the root path" do
                       email: "jane@jane.com",
                       password: "boom")
     visit root_path
-    click_on "Sign in"
+    click_on "Log in"
   end
 
   scenario "they can navigate to the login page and log in" do
     fill_in "email", with: "jane@jane.com"
     fill_in "password", with: "boom"
-    click_on "Log in"
+    within("form") do
+      click_on "Log in"
+    end
 
     expect(current_path).to eql(dashboard_path)
     expect(page).to have_css(".navbar", text: "Logged in as Jane Doe")
+    expect(page).to have_content("Successfully logged in")
   end
 
   scenario "they see an error message if they leave the email blank" do
     fill_in "password", with: "boom"
-    click_on "Log in"
+    within("form") do
+      click_on "Log in"
+    end
 
     expect(page).to have_content("Email and password combination do not exist")
+    expect(page).not_to have_content("Successfully logged in")
   end
 
   scenario "they see an error message if they leave the password blank" do
     fill_in "email", with: "jane@jane.com"
-    click_on "Log in"
+    within("form") do
+      click_on "Log in"
+    end
 
     expect(page).to have_content("Email and password combination do not exist")
+    expect(page).not_to have_content("Successfully logged in")
   end
 
   scenario "they see an error message if they leave both the email and password blank" do
-    click_on "Log in"
+    within("form") do
+      click_on "Log in"
+    end
 
     expect(page).to have_content("Email and password combination do not exist")
+    expect(page).not_to have_content("Successfully logged in")
   end
 
   scenario "they see an error message if they input an invalid email/password combination" do
     fill_in "email", with: "john@john.com"
     fill_in "password", with: "boom"
-    click_on "Log in"
+    within("form") do
+      click_on "Log in"
+    end
 
     expect(page).to have_content("Email and password combination do not exist")
+    expect(page).not_to have_content("Successfully logged in")
   end
 
   scenario "they can navigate to the Create Account page and create an account" do
@@ -62,11 +77,12 @@ feature "When a visitor visits the root path" do
 
     expect(current_path).to eql(dashboard_path)
     expect(page).to have_css(".navbar", text: "Logged in as John Smith")
+    expect(page).to have_content("Successfully logged in")
     expect(page).to have_content("Account Information")
     expect(page).to have_content("First name: John")
     expect(page).to have_content("Last name: Smith")
     expect(page).to have_content("Email: john@john.com")
-    expect(page).not_to have_selector(:link_or_button, text: "Sign in")
+    expect(page).not_to have_selector(:link_or_button, text: "Log in")
     expect(page).to have_selector(:link_or_button, text: "Log out")
   end
 
@@ -80,6 +96,7 @@ feature "When a visitor visits the root path" do
     expect(page).to have_content("Last name can't be blank")
     expect(page).to have_content("Email can't be blank")
     expect(page).to have_content("Password can't be blank")
+    expect(page).not_to have_content("Successfully logged in")
   end
 
   scenario "they see an error message if a customer account already exists for that email" do
@@ -96,5 +113,6 @@ feature "When a visitor visits the root path" do
 
     expect(current_path).to eql(customers_path)
     expect(page).to have_content("Email has already been taken")
+    expect(page).not_to have_content("Successfully logged in")
   end
 end
