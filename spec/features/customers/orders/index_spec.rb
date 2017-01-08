6bdeeb5e51a61c0a9851fd6a965d3e6e03a1de19
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-01-07 21:34:15 -0700

    Update customer orders spec for logged in user

--- spec/features/customers/orders/index_spec.rb
+++ spec/features/customers/orders/index_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe "Viewing a customer's previous orders" do
+
   context "a customer has no orders" do
     scenario "a pleasant message appears" do
-      customer = create(:customer)
+      customer = log_in_as_customer
       visit "/orders"
 
       expect(page).to have_content "You have not placed any orders."
@@ -12,8 +13,8 @@ describe "Viewing a customer's previous orders" do
 
   context "a customer has previous orders" do
     scenario "those orders appear as a list" do
-      customer = create(:customer)
-      order_1, order_2 = create_list(:order, 2)
+      customer = log_in_as_customer
+      order_1, order_2, order_3 = create_list(:order, 3)
       customer.orders = [order_1, order_2]
       visit "/orders"
 
@@ -21,6 +22,7 @@ describe "Viewing a customer's previous orders" do
       expect(page).to have_content order_1.date
       expect(page).to have_content "$#{order_2.total_price_in_dollars}"
       expect(page).to have_content order_2.date
+      expect(page).not_to have_content "$#{order_3.total_price_in_dollars}"
     end
   end
 end
