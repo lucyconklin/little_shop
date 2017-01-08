1e124abcad9b951fe1261f1c5ff4dc4fa286f79d
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-01-08 01:31:22 -0700

    Display detailed item information for an order

--- app/models/order.rb
+++ app/models/order.rb
@@ -17,4 +17,27 @@ class Order < ApplicationRecord
   def date
     created_at.strftime("%e %b %Y")
   end
+
+  def items_and_quantities
+    items.inject(Hash.new(0)) do |hash, item|
+      hash[item] += 1
+      hash
+    end
+  end
+
+  def status_name
+    status.name.capitalize
+  end
+
+  def completed_or_cancelled?
+    status.name == "completed" || status.name == "cancelled"
+  end
+
+  def display_submitted_at
+    created_at.strftime('%e %b %Y at %l:%M %p')
+  end
+
+  def display_updated_at
+    created_at.strftime('%e %b %Y at %l:%M %p')
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -14,7 +14,7 @@ describe Order do
 
   describe "#total_price_in_dollars" do
     it "returns the price in dollars" do
-      order = create(:order, total_price_in_cents: 12_34)
+      order = create(:all_new_order, total_price_in_cents: 12_34)
 
       expect(order.total_price_in_dollars).to eq 12.34
     end
@@ -23,9 +23,72 @@ describe Order do
   describe "#date" do
     it "returns a well-formatted date" do
       order_date = DateTime.new(2017, 2, 22, 8)
-      order = create(:order, created_at: order_date)
+      order = create(:all_new_order, created_at: order_date)
 
       expect(order.date).to eq "22 Feb 2017"
+    end
+  end
+
+  describe "#items_and_quantities" do
+    it "returns the items and the quantities" do
+      item_1, item_2 = create_list(:item, 2)
+      items = [item_1, item_1, item_1, item_2]
+      order = create(:all_new_order, items: items)
+
+      expect(order.items_and_quantities).to include(item_1 => 3, item_2 => 1)
+    end
+  end
+
+  describe "#status_name" do
+    it "returns the name of the status" do
+      status = create(:status, name: "paid")
+      order = create(:all_new_order, status: status)
+
+      expect(order.status_name).to eq "Paid"
+    end
+  end
+
+  describe "#completed_or_cancelled?" do
+    let(:completed) { create(:status, name: "completed") }
+    let(:cancelled) { create(:status, name: "cancelled") }
+    let(:paid) { create(:status, name: "paid") }
+
+    it "returns true when the order has a status of completed" do
+      order = create(:all_new_order, status: completed)
+
+      expect(order.completed_or_cancelled?).to be true
+    end
+
+    it "returns true when the order has a status of cancelled" do
+      order = create(:all_new_order, status: cancelled)
+
+      expect(order.completed_or_cancelled?).to be true
+    end
+
+    it "returns false when the status is not completed or cancelled" do
+      order = create(:all_new_order, status: paid)
+
+      expect(order.completed_or_cancelled?).to be false
+    end
+  end
+
+  describe "#display_submitted_at" do
+    let(:order) { create(:all_new_order) }
+    let(:submitted_at) { DateTime.new(2017, 2, 22, 8, 22) }
+    it "returns a nicely formatted date-time" do
+      order.update(created_at: submitted_at)
+
+      expect(order.display_submitted_at).to eq "22 Feb 2017 at  8:22 AM"
+    end
+  end
+
+  describe "#display_updated_at" do
+    let(:order) { create(:all_new_order) }
+    let(:updated_at) { DateTime.new(2017, 2, 24, 8, 24) }
+    it "returns a nicely formatted date-time" do
+      order.update(created_at: updated_at)
+
+      expect(order.display_updated_at).to eq "24 Feb 2017 at  8:24 AM"
     end
   end
 end
