bbab60e2d00581639518d186a31a5e463146eb4a
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-01-08 01:30:21 -0700

    Save the price history of an item when it is ordered

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -5,4 +5,22 @@ describe OrderItem do
     it { is_expected.to belong_to(:order) }
     it { is_expected.to belong_to(:item) }
   end
+
+  describe "before saving" do
+    context "saves the item quantity and price" do
+      it "returns the price of the item in cents" do
+        items = create_list(:item, 2)
+        order = create(:all_new_order, items: items)
+        order_items = order.order_items
+
+        expect(order_items.pluck(:item_price_in_cents)).to match_array items.map(&:price_in_cents)
+      end
+    end
+  end
+
+  describe "#item_title" do
+    it "returns the title of the item" do
+
+    end
+  end
 end
