8332b44a9b5517f03cd9bbe4aa6668f24b0f89fc
Robbie Smith <robert.smith160@icloud.com>
2017-01-08 19:33:05 -0700

    Refactor spec/customers/...login_spec.rb

--- spec/features/customers/customer_can_login_spec.rb
+++ spec/features/customers/customer_can_login_spec.rb
@@ -2,64 +2,39 @@ require "rails_helper"
 
 feature "When a customer visits the root path" do
   before do
-    Customer.create!( first_name: "Jane",
-      last_name: "Doe",
-      email: "jane@jane.com",
-    password: "boom")
+    create(:customer, first_name: "Jane", last_name: "Doe", email: "jane@jane.com")
     visit root_path
-    click_on "Log in"
   end
 
-  scenario "they can navigate to the login page and log in" do
-    fill_in "email", with: "jane@jane.com"
-    fill_in "password", with: "boom"
-    within("form") do
-      click_on "Log in"
-    end
+  scenario "the customer should see a log in button or link" do
+    expect(page).to have_selector(:link_or_button, "Log in")
+  end
 
-    expect(current_path).to eql(dashboard_path)
-    expect(page).to have_css(".navbar", text: "Logged in as Jane Doe")
+  scenario "the customer should be redirected to the login path after clicking log in" do
+    click_on "Log in"
+
+    expect(current_path).to eql(login_path)
+  end
+
+  scenario "the user is redirected to their dashboard page upon logging in" do
+    click_on "Log in"
+    login
+
+    expect(page).to have_current_path(dashboard_path)
+  end
+
+  scenario "the user should see a successfully logged in message after logging in" do
+    click_on "Log in"
+    login
+
     expect(page).to have_content("Successfully logged in")
   end
 
-  scenario "they see an error message if they leave the email blank" do
-    fill_in "password", with: "boom"
-    within("form") do
-      click_on "Log in"
-    end
+  scenario "the user should see a message that tells them they are logged in " do
+    click_on "Log in"
+    login
 
-    expect(page).to have_content("Email and password combination do not exist")
-    expect(page).not_to have_content("Successfully logged in")
-  end
-
-  scenario "they see an error message if they leave the password blank" do
-    fill_in "email", with: "jane@jane.com"
-    within("form") do
-      click_on "Log in"
-    end
-
-    expect(page).to have_content("Email and password combination do not exist")
-    expect(page).not_to have_content("Successfully logged in")
-  end
-
-  scenario "they see an error message if they leave both the email and password blank" do
-    within("form") do
-      click_on "Log in"
-    end
-
-    expect(page).to have_content("Email and password combination do not exist")
-    expect(page).not_to have_content("Successfully logged in")
-  end
-
-  scenario "they see an error message if they input an invalid email/password combination" do
-    fill_in "email", with: "john@john.com"
-    fill_in "password", with: "boom"
-    within("form") do
-      click_on "Log in"
-    end
-
-    expect(page).to have_content("Email and password combination do not exist")
-    expect(page).not_to have_content("Successfully logged in")
+    expect(page).to have_css(".navbar", text: "Logged in as Jane Doe")
   end
 
 end
