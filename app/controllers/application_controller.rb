bbc4a7cc9c930abbe984edfff2210f6fcf02fd52
Robbie Smith <Robbie-Smith@users.noreply.github.com>
2017-01-09 10:29:17 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_customer
-    @current_customer ||= Customer.find(session[:customer_id]) if !session[:customer_id].nil?
+    @current_customer ||= Customer.find(session[:customer_id]) if session[:customer_id]
   end
 
 end
