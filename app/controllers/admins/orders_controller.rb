23991ab8a4d24466d8927a79a33aa8769874dedb
Lucy Conklin <lucy.conklin@gmail.com>
2017-01-10 13:06:39 -0700

    Cancel button functioning, need to add classes to pass test

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -7,7 +7,7 @@ class Admins::OrdersController < ApplicationController
   def update
     order = Order.find(params["order_id"])
     order.status = Status.find_by(name: params["status"])
-    byebug
+    order.save
     redirect_to admin_dashboard_path
     # if Status.all.include?(params[:status])
     # order.save
