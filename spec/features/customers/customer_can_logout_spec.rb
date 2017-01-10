5d428d4da25e8bd43bc56396cd0f09d19b9d69ad
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-01-09 20:45:44 -0700

    Clear cart contents on logout

--- spec/features/customers/customer_can_logout_spec.rb
+++ spec/features/customers/customer_can_logout_spec.rb
@@ -1,29 +1,36 @@
 require "rails_helper"
 
-feature "When a customer is logged in" do
+feature "Log as as a customer" do
   before do
+    add_three_items_to_cart
     logged_in_as_customer
     visit root_path
   end
 
-  scenario "the customer should view a logged in as message" do
-    expect(page).to have_css(".navbar", text: "Logged in as Jane Doe")
+  context "when the customer is logged in" do
+    scenario "the customer should view a logged in as message" do
+      expect(page).to have_css(".navbar", text: "Logged in as Jane Doe")
+    end
+
+    scenario "the customer should view a log out button" do
+      expect(page).to have_selector(:link_or_button, text: "Log out")
+    end
   end
 
-  scenario "the customer should view a log out button" do
-    expect(page).to have_selector(:link_or_button, text: "Log out")
-  end
+  context "when the customer has logged out" do
+    before { click_on "Log out" }
 
-  scenario "after the customer clicks on the log out button or link they should see a logged out message" do
-    click_on "Log out"
+    scenario "they should see a logged out message" do
+      expect(page).to have_content("Successfully logged out")
+    end
 
-    expect(page).to have_content("Successfully logged out")
-  end
+    scenario "they should then see a log in button or link" do
+      expect(page).not_to have_selector(:link_or_button, text: "Log out")
+      expect(page).to have_selector(:link_or_button, text: "Log in")
+    end
 
-  scenario "after the customer clicks on the log out button or link they should then see a log in button or link" do
-    click_on "Log out"
-
-    expect(page).not_to have_selector(:link_or_button, text: "Log out")
-    expect(page).to have_selector(:link_or_button, text: "Log in")
+    scenario "there should not be anything in their cart" do
+      expect(page).to have_css(".cart_counter", text: "0")
+    end
   end
 end
