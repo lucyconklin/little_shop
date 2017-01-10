b3bd13f25c82566079cb912e1ee4ef9a25960436
Caroline Powell <carpowell10@gmail.com>
2017-01-09 18:57:36 -0700

    Add order status filter to customer orders table

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -1,11 +1,23 @@
 class Customers::OrdersController < Customers::BaseController
-  before_action :set_orders
+  before_action :customer_orders
 
-  def set_orders
-    @orders = current_customer.orders
+  def customer_orders
+    current_customer.orders
   end
 
   def index
+    @status_filter = params[:status_filter]
+    @statuses = Status.all
+
+    if @status_filter.nil?
+      @orders = customer_orders.most_recent
+    elsif @statuses.pluck(:name).include?(@status_filter) == false
+      render file: "/public/404"
+    else
+      status = Status.find_by(name: @status_filter)
+      # @orders = status.orders.most_recent
+      @orders = customer_orders.where(status: status).most_recent
+    end
   end
 
   def show
