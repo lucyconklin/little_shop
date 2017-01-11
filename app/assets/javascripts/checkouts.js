8cbf27fb3ec7ce1a8e3ba8d3fee26377787f42f3
Lucy Conklin <lucy.conklin@gmail.com>
2017-01-11 15:29:56 -0700

    Stripe card processing functional

--- app/assets/javascripts/checkouts.js
+++ app/assets/javascripts/checkouts.js
@@ -26,11 +26,16 @@ function stripeResponseHandler(status, response) {
 
     // Get the token ID:
     var token = response.id;
+    console.log(response);
 
     // Insert the token ID into the form so it gets submitted to the server:
     $form.append($('<input type="hidden" name="stripeToken">').val(token));
+    $form.append($('<input type="hidden" name="card_brand">').val(response.card.brand));
+    $form.append($('<input type="hidden" name="card_last4">').val(response.card.last4));
+    $form.append($('<input type="hidden" name="card_exp_month">').val(response.card.exp_month));
+    $form.append($('<input type="hidden" name="card_exp_year">').val(response.card.exp_year));
 
     // Submit the form:
-    // $form.get(0).submit();
+    $form.get(0).submit();
   }
 };
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,15 @@ class Customer < ApplicationRecord
   validates :password_digest, presence: true
 
   has_many :orders
+
+  def stripe_customer
+    if stripe_id?
+      Stripe::Customer.retrieve(stripe_id)
+    else
+      byebug
+      stripe_customer = Stripe::Customer.create(email: email)
+      update(stripe_id: stripe_customer.id)
+      stripe_customer
+    end
+  end
 end
